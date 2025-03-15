502fca42cc6a8622d1052ae3a9d2826cb751c780
Alejo Herrera <alejotomasherrera@gmail.com>
2025-03-15 00:08:12 -0300

    Creacion de proyectos de maven

---
+++ servicio-eurekaServer/src/test/java/digitalMonyHouse/servicio_eurekaServer/ServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package digitalMonyHouse.servicio_eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ servicio-transacciones/src/test/java/digitalMonyHouse/servicio_transacciones/ServicioTransaccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package digitalMonyHouse.servicio_transacciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioTransaccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
